13cfd91ac289bbce2da6be68be0b691edc46ffdc
zwazoo <liudmilla.kharchenko@gmail.com>
2018-12-10 17:04:23 +0200

    init

---
+++ js/main.js
@@ -0,0 +1,15 @@
+;(function ($) {
+    "use strict";
+
+    $(function() {
+        $('.ba-team-slider').slick({
+            dots:true,
+            slide:".ba-team-slider__item",
+            prevArrow:".ba-slider__prev",
+            nextArrow:".ba-slider__next",
+
+        }
+        );
+    });
+
+})(jQuery);
